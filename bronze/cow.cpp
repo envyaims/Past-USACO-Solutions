20af0a616eda566b6833e122681f025e6b1dca70
envyaims <79723780+envyaims@users.noreply.github.com>
2021-08-16 21:36:53 -0400

    Add files via upload

---
+++ bronze/cow.cpp
@@ -0,0 +1,207 @@
+// Problem: Problem 2. COW
+// Contest: USACO - USACO 2015 February Contest, Bronze
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=527
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+// Input: stdin   
+// Output: stdout   
+// Attempted: 2021-08-16 15:30:16 EST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+ 
+#ifdef LOCAL
+#define debug(...) cerr << #__VA_ARGS__ << ":", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+
+inline namespace fastio {
+	const int BSZ = 1<<15; 
+	char ibuf[BSZ]; int ipos, ilen;
+	char nc() {
+		if (ipos == ilen) {
+			ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);
+			if (!ilen) return EOF;
+		}
+		return ibuf[ipos++];
+	}
+	template<class T> void ri(T& x) { // read int or ll
+		char ch; int sgn = 1;
+		while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;
+		x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');
+		x *= sgn;
+	}
+	template<class T> void rc(T& x){ // read char
+		x = nc();
+		while(isspace(x)){
+			x = nc();
+		}
+	}
+	template<class T, class... Ts> void ri(T& t, Ts&... ts) { 
+		ri(t); ri(ts...); } // read ints
+	template<class str>void rs(str& x) { // read str
+		char ch; while (isspace(ch = nc()));
+		do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);
+	}
+}
+
+typedef long long ll;
+const ll INF = 1e18;
+const int MOD = 1e9+7, MN = 1e5 + 5;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+const int dx[4] = {1,-1,0,0};
+const int dy[4] = {0,0,-1,1};
+
+void doStuff(){
+	freopen("cow.in","r",stdin);
+	freopen("cow.out","w",stdout);
+	int n; ri(n);
+	// vector<int> ccount(n,0);
+	// vector<int> wcount(n,0);
+	// vector<int> opos;
+	// ll ans = 0;
+	// for(int i = 0; i < n; i++){
+		// char a; rc(a);
+		// if(a == 'C'){
+			// for(int j = i; j < n; j++){
+				// ccount[j]++;
+			// }
+		// }
+		// else if(a == 'W'){
+			// for(int j = 0; j < i; j++){
+				// wcount[j]++;
+			// }
+		// }
+		// else{
+			// ans += ccount[i] * wcount[i];
+		// }
+	// }
+	// print(ans);
+	
+	string s; rs(s);
+	ll ans = 0;
+	for(int i = 1; i < n-1; i++){
+		if(s[i] == 'O'){
+			ans += count(s.begin(),s.begin()+i,'C') * count(s.begin()+i,s.end(),'W');
+		}
+	}
+	print(ans);
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	doStuff();
+	// int t; ri(t);
+	// while(t--){
+		// doStuff();
+	// }
+    
+    
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+    return 0;
+}
+
+// you should actually read the stuff at the bottom and not submit the same wrong code
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing
+	* maybe just stop being dumb lol
+*/
---
+++ bronze/moocrypt.cpp
@@ -0,0 +1,230 @@
+// Problem: Problem 1. Moocryption
+// Contest: USACO - USACO 2015 US Open, Bronze
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=545
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+// Input: file moocrypt.in  
+// Output: file moocrypt.out  
+// Attempted: 2021-08-16 14:13:55 EST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+ 
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+ 
+#ifdef LOCAL
+#define debug(...) cerr << #__VA_ARGS__ << ":", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+
+inline namespace fastio {
+	const int BSZ = 1<<15; 
+	char ibuf[BSZ]; int ipos, ilen;
+	char nc() {
+		if (ipos == ilen) {
+			ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);
+			if (!ilen) return EOF;
+		}
+		return ibuf[ipos++];
+	}
+	template<class T> void ri(T& x) { // read int or ll
+		char ch; int sgn = 1;
+		while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;
+		x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');
+		x *= sgn;
+	}
+	template<class T> void rc(T& x){ // read char
+		x = nc();
+		while(isspace(x)){
+			x = nc();
+		}
+	}
+	template<class T, class... Ts> void ri(T& t, Ts&... ts) { 
+		ri(t); ri(ts...); } // read ints
+	template<class str>void rs(str& x) { // read str
+		char ch; while (isspace(ch = nc()));
+		do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);
+	}
+}
+
+typedef long long ll;
+const ll INF = 1e18;
+const int MOD = 1e9+7, MN = 1e5 + 5;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+const int dx[4] = {1,-1,0,0};
+const int dy[4] = {0,0,-1,1};
+int n, m;
+char grid[53][53];
+map<pair<char,char>,int> possible;
+// first = M, second = O
+
+void doStuff(){
+	freopen("moocrypt.in","r",stdin);
+	freopen("moocrypt.out","w",stdout);
+	ri(n,m);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			rc(grid[i][j]);
+		}
+	}
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			// horizontally
+			if(j < m-2 && grid[i][j+1] == grid[i][j+2]){
+				possible[mp(grid[i][j],grid[i][j+1])]++;
+			}
+			// vertically
+			if(i < n-2 && grid[i+1][j] == grid[i+2][j]){
+				possible[mp(grid[i][j],grid[i+1][j])]++;
+			}
+			// left diagonally
+			if(i < n-2 && j < m-2 && grid[i+1][j+1] == grid[i+2][j+2]){
+				possible[mp(grid[i][j],grid[i+1][j+1])]++;
+			}
+			// horizontally backwards
+			if(j > 1 && grid[i][j-1] == grid[i][j-2]){
+				possible[mp(grid[i][j],grid[i][j-1])]++;
+			}
+			// vertically backwards
+			if(i > 1 && grid[i-1][j] == grid[i-2][j]){
+				possible[mp(grid[i][j],grid[i-1][j])]++;
+			}
+			// left diagonally backwards
+			if(i > 1 && j > 1 && grid[i-1][j-1] == grid[i-2][j-2]){
+				possible[mp(grid[i][j],grid[i-1][j-1])]++;
+			}
+			// right diagonally
+			if(i < n-2 && j > 1 && grid[i+1][j-1] == grid[i+2][j-2]){
+				possible[mp(grid[i][j],grid[i+1][j-1])]++;
+			}
+			// right diagonally backwards
+			if(j < m-2 && i > 1 && grid[i-1][j+1] == grid[i-2][j+2]){
+				possible[mp(grid[i][j],grid[i-1][j+1])]++;
+			}
+		}
+	}
+	
+	int ans = 0;
+	for(auto i: possible){
+		if(i.F.F != i.F.S && i.F.F != 'M' && i.F.S != 'O'){
+			ans = max(ans,i.S);
+		}
+	}
+	print(ans);
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	doStuff();
+	// int t; ri(t);
+	// while(t--){
+		// doStuff();
+	// }
+    
+    
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+    return 0;
+}
+
+// you should actually read the stuff at the bottom and not submit the same wrong code
+
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing
+	* maybe just stop being dumb lol
+*/
