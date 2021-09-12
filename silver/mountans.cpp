0679d6158eee8103ccba89a361400d7651b7d7a6
envyaims <79723780+envyaims@users.noreply.github.com>
2021-09-12 16:42:09 -0400

    Add files via upload

---
+++ silver/mountans.cpp
@@ -0,0 +1,79 @@
+// Author: envy
+// Problem: Problem 3. Mountain View from USACO - USACO 2019 January Contest, Silver
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=896
+// Limits: 4000 ms, 256 MB
+// Attempted: 2021-09-12 12:42:50 EST
+
+#ifndef LOCAL
+#define debug(...) 0
+#include <bits/stdc++.h>
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#include "C:\programmingfunnyxd\includeordie.h"
+#endif
+using namespace std;
+inline namespace readio { const int BSZ = 1<<15; char ibuf[BSZ]; int ipos, ilen;char nc() {if (ipos == ilen) {ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);if (!ilen) return EOF;}return ibuf[ipos++];}
+template<class T> void ri(T& x) {char ch; int sgn = 1;while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');x *= sgn;}
+template<class T> void rc(T& x){ x = nc();while(isspace(x))x = nc();}template<class T, class... Ts> void ri(T& t, Ts&... ts) {ri(t);ri(ts...);}
+template<class str>void rs(str& x) { char ch; while (isspace(ch = nc()));do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);}}
+template<typename T> void max_self(T& a, T b){a = max(a,b);}template<typename T> void min_self(T& a, T b){a = min(a,b);}
+struct minsecond{bool operator()(pair<int,int> a, pair<int,int> b){return a.second > b.second;}};
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;template<typename T> using maxpq = priority_queue<T>;
+typedef priority_queue<pair<int,int>, vector<pair<int,int>>, minsecond> secondpq;
+typedef long long ll; const ll INF = 1e18; const int MOD = 1e9+7;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+#define fillarr(arr,n,am) fill(arr[0],arr[n],am)
+#define readarr(arr) for(int &i: arr)ri(i);
+#define readchararr(arr)for(int &i: arr)rc(i);
+#define readgrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)ri(arr[i][j]);
+#define readchargrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)rc(arr[i][j]);
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,-1,1};
+#define int long long // fight me
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b/=2;a=(a*a)%m;}return ans;}
+
+bool customsort(pair<int,int> a, pair<int,int> b){
+	return a.F == b.F ? a.S > b.S : a.F < b.F;
+}
+
+void doStuff(){
+	int n; ri(n);
+	vector<pair<int,int>> nums(n);
+	// F is left bound, S is right bound
+	for(int i = 0; i < n; i++){
+		int a,b; ri(a,b);
+		nums[i] = mp(a-b,a+b);
+	}
+	sort(all(nums),customsort);
+	
+	int ans = n;
+	int maxnow = nums[0].S;
+	for(int i = 1; i < n; i++){
+		if(nums[i].S <= maxnow){
+			ans--;
+		}
+		else if(nums[i].S > maxnow){
+			maxnow = nums[i].S;
+		}
+	}
+	print(ans);
+	
+}
+
+signed main(){
+	freopen("mountains.in","r",stdin);
+	freopen("mountains.out","w",stdout);
+	ios_base::sync_with_stdio(false);cin.tie(0);int t = 1;
+	// ri(t);
+	while(t--)doStuff();
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
---
+++ silver/rental.cpp
@@ -0,0 +1,100 @@
+// Author: envy
+// Problem: Problem 2. Rental Service from USACO - USACO 2018 January Contest, Silver
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=787
+// Limits: 4000 ms, 256 MB
+// Attempted: 2021-09-12 08:54:09 EST
+
+#ifndef LOCAL
+#define debug(...) 0
+#include <bits/stdc++.h>
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#include "C:\programmingfunnyxd\includeordie.h"
+#endif
+using namespace std;
+inline namespace readio { const int BSZ = 1<<15; char ibuf[BSZ]; int ipos, ilen;char nc() {if (ipos == ilen) {ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);if (!ilen) return EOF;}return ibuf[ipos++];}
+template<class T> void ri(T& x) {char ch; int sgn = 1;while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');x *= sgn;}
+template<class T> void rc(T& x){ x = nc();while(isspace(x))x = nc();}template<class T, class... Ts> void ri(T& t, Ts&... ts) {ri(t);ri(ts...);}
+template<class str>void rs(str& x) { char ch; while (isspace(ch = nc()));do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);}}
+template<typename T> void max_self(T& a, T b){a = max(a,b);}template<typename T> void min_self(T& a, T b){a = min(a,b);}
+struct minsecond{bool operator()(pair<int,int> a, pair<int,int> b){return a.second > b.second;}};
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;template<typename T> using maxpq = priority_queue<T>;
+typedef priority_queue<pair<int,int>, vector<pair<int,int>>, minsecond> secondpq;
+typedef long long ll; const ll INF = 1e18; const int MOD = 1e9+7;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+#define fillarr(arr,n,am) fill(arr[0],arr[n],am)
+#define readarr(arr) for(int &i: arr)ri(i);
+#define readchararr(arr)for(int &i: arr)rc(i);
+#define readgrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)ri(arr[i][j]);
+#define readchargrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)rc(arr[i][j]);
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,-1,1};
+#define int long long // fight me
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b/=2;a=(a*a)%m;}return ans;}
+
+void doStuff(){
+	int n,m,r; ri(n,m,r);
+	vector<int> cows(n);
+	readarr(cows);
+	sort(all(cows));
+	vector<int> prefixcows(n+1);
+	
+	for(int i = 0; i < n; i++){
+		prefixcows[i+1] = cows[i] + prefixcows[i];
+	}
+	
+	vector<pair<int,int>> milk(m);
+	for(int i = 0; i < m; i++){
+		int a,b; ri(a,b);
+		milk[i] = mp(b,a);
+	}
+	sort(all(milk),greater<pair<int,int>>());
+	vector<int> value(m+1);
+	vector<int> amount(m+1);
+	for(int i = 0; i < m; i++){
+		value[i+1] = value[i] + milk[i].S * milk[i].F;
+		amount[i+1] = amount[i] + milk[i].S;
+	}
+	
+	vector<int> sell(r);
+	readarr(sell);
+	sort(all(sell),greater<int>());
+	vector<int> prefixsell(r+1);
+	for(int i = 0; i < r; i++){
+		prefixsell[i+1] = prefixsell[i] + sell[i];
+	}
+	int ans = 0;
+	for(int i = 0; i <= n; i++){
+		int moners = 0;
+		// cows 0 to i inclusive will be sold!
+		int curmilk = prefixcows[n] - prefixcows[i];
+		auto it = upper_bound(all(amount), curmilk) - amount.begin();
+		curmilk -= amount[it-1];
+		moners += value[it-1];
+		moners += curmilk * milk[it-1].F;
+		moners += prefixsell[min(i,r)];
+		ans = max(ans,moners);
+		
+	}
+	print(ans);
+	
+	
+}
+
+signed main(){
+	freopen("rental.in","r",stdin);
+	freopen("rental.out","w",stdout);
+	ios_base::sync_with_stdio(false);cin.tie(0);int t = 1;
+	// ri(t);
+	while(t--)doStuff();
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
+}
