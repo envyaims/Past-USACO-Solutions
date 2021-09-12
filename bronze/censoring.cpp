749aeaf57bf4356a647f9c1ddd0e37a05c699ec4
envyaims <79723780+envyaims@users.noreply.github.com>
2021-09-12 18:02:00 -0400

    Update censoring.cpp

--- bronze/censoring.cpp
+++ bronze/censoring.cpp
@@ -1,59 +1,64 @@
-// Problem: Problem 1. Censoring (Bronze)
-// Contest: USACO - USACO 2015 February Contest, Bronze
+// Author: envy
+// Problem: Problem 1. Censoring (Bronze) from USACO - USACO 2015 February Contest, Bronze
 // URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=526
-// Memory Limit: 256 MB
-// Time Limit: 4000 ms
-// 
-// Powered by CP Editor (https://cpeditor.org)
+// Limits: 4000 ms, 256 MB
+// Attempted: 2021-09-12 17:57:56 EST
 
+#ifndef LOCAL
+#define debug(...) 0
 #include <bits/stdc++.h>
-
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#include "C:\programmingfunnyxd\includeordie.h"
+#endif
 using namespace std;
-
-#define ll long long
-#define INF INT_MAX
-#define all(x) x.begin(), x.end() // for vectors, etc
-#define All(x,l) = x, x+l; // for standard arrays
-#define pb(x) push_back(x);
-#define debug(x) cout<<#x<<" = "<<x<<endl
-#define print(x) cout<<x<<endl
+inline namespace readio { const int BSZ = 1<<15; char ibuf[BSZ]; int ipos, ilen;char nc() {if (ipos == ilen) {ipos = 0; ilen = fread(ibuf,1,BSZ,stdin);if (!ilen) return EOF;}return ibuf[ipos++];}
+template<class T> void ri(T& x) {char ch; int sgn = 1;while (!isdigit(ch = nc())) if (ch == '-') sgn *= -1;x = ch-'0'; while (isdigit(ch = nc())) x = x*10+(ch-'0');x *= sgn;}
+template<class T> void rc(T& x){ x = nc();while(isspace(x))x = nc();}template<class T, class... Ts> void ri(T& t, Ts&... ts) {ri(t);ri(ts...);}
+template<class str>void rs(str& x) { char ch; while (isspace(ch = nc()));do { x += ch; } while (!isspace(ch = nc()) && ch != EOF);}}
+template<typename T> void max_self(T& a, T b){a = max(a,b);}template<typename T> void min_self(T& a, T b){a = min(a,b);}
+struct minsecond{bool operator()(pair<int,int> a, pair<int,int> b){return a.second > b.second;}};
+template<typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;template<typename T> using maxpq = priority_queue<T>;
+typedef priority_queue<pair<int,int>, vector<pair<int,int>>, minsecond> secondpq;
+typedef long long ll; const ll INF = 1e18; const int MOD = 1e9+7;
+#define all(x) x.begin(), x.end()
+#define pb(x) push_back(x)
+#define print(x) cout<<x<<"\n"
 #define mp(x,y) make_pair(x,y)
+#define np(x) next_permutation(all(x))
+#define F first
+#define S second
+#define sz(v) int(v.size())
+#define bpop(x) __builtin_popcount(x)
+#define fillarr(arr,n,am) fill(arr[0],arr[n],am)
+#define readarr(arr) for(int &i: arr)ri(i);
+#define readchararr(arr)for(int &i: arr)rc(i);
+#define readgrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)ri(arr[i][j]);
+#define readchargrid(arr, n, m)for(int i = 0; i < n; i++)for(int j = 0; j < m; j++)rc(arr[i][j]);
+const int dx[4] = {1,-1,0,0}, dy[4] = {0,0,-1,1};
+#define int long long // fight me
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b/=2;a=(a*a)%m;}return ans;}
 
-template<typename T>
-vector<string> split(T str, char sep){
-	vector<string> output;
-	size_t start = 0, end = 0;
-	while((end = str.find(sep,start)) != string::npos){
-		if(end != start)output.pb(str.substr(start,end-start));
-		start = end+1;
+void doStuff(){
+	string init, del; cin>>init>>del;
+	string build;
+	for(int i = 0; i < init.length(); i++){
+		build += init[i];
+		if(build.length() >= del.length() && build.substr(build.length() - del.length()) == del){
+			build.resize(build.length() - del.length());
+		}
 	}
-	
-	if(end != start)output.pb(str.substr(start));
-	return output;
+	print(build);
 	
 }
 
-void solve(){
-	ifstream fin("censor.in");
-	ofstream fout("censor.out");
-	string init,del;
-	getline(fin,init);
-	getline(fin,del);
-	while(init.find(del) != string::npos){
-		int ind = init.find(del);
-		init = init.substr(0, ind) + init.substr(ind+del.length());
-	}
-	fout<<init<<endl;
-	 
-	
-	
-}
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	solve();
-    
-
-    return 0;
+signed main(){
+	freopen("censor.in","r",stdin);
+	freopen("censor.out","w",stdout);
+	ios_base::sync_with_stdio(false);cin.tie(0);int t = 1;
+	// ri(t);
+	while(t--)doStuff();
+    #ifdef LOCAL
+    cerr << "Runtime: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+	#endif
 }
