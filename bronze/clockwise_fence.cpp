81fc5278d84304194535d83bd3d36348b4824577
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-15 20:10:25 -0500

    Update clockwise_fence.cpp

--- bronze/clockwise_fence.cpp
+++ bronze/clockwise_fence.cpp
@@ -1,77 +1,138 @@
+// Author: envy <3 uwu
 // Problem: Problem 3. Clockwise Fence
-// Contest: USACO - USACO 2021 February Contest, Bronze
-// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=1109
-// Memory Limit: 256 MB
-// Time Limit: 4000 ms
-// 
-// ALL TEST CASES PASSED. 7/17/2021
+// Attempted: 2021-12-15 19:08:49 EST
 
 #include <bits/stdc++.h>
-
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#endif
 using namespace std;
+#define int long long
 
-#define ll long long
-#define INF INT_MAX
-#define all(x) x.begin(), x.end() // for vectors, etc
-#define All(x,l) x, x+l // for standard arrays, l = length
-#define pb(x) push_back(x)
-#define debug(x) cout<<#x<<" = "<<x<<endl
-#define print(x) cout<<x<<endl
-#define mp(x,y) make_pair(x,y)
-#define cpy(x,y) copy(begin(x),end(x),begin(y)) // copy standard arrays x -> y
+int n;
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
 
-template<typename T>
-vector<string> split(T str, char sep){
-	vector<string> output;
-	size_t start = 0, end = 0;
-	while((end = str.find(sep,start)) != string::npos){
-		if(end != start)output.pb(str.substr(start,end-start));
-		start = end+1;
+bool okie(int r, int c){
+	if(r < 0 || r >= 500 || c < 0 || c >= 500)return false;
+	return true;
+}
+
+bool ok;
+
+void ff(int r, int c, vector<vector<bool>>& fence, vector<vector<bool>>& visited){
+	if(!ok)return;
+	if(!okie(r,c)){
+		ok = false;
+		return;
 	}
-	
-	if(end != start)output.pb(str.substr(start));
-	return output;
-	
+	if(visited[r][c] || fence[r][c])return;
+	visited[r][c] = true;
+	for(int i = 0; i < 4; i++){
+		ff(dx[i]+r,dy[i]+c,fence,visited);
+	}
 }
-
-int crossproduct(pair<int,int> p1, pair<int,int> p2){
-	return p1.first * p2.second - p2.first * p1.second;
-}
-
-
-void solve(){
-	int n;
-	cin>>n;
-	for(int test = 0; test < n; test++){
-		string path;
-		cin>>path;
-		pair<int,int> location = mp(0,0);
-		int output = 0;
-		for(char i: path){
-			pair<int, int> newlocation;
-			if(i == 'N')newlocation = mp(location.first,location.second+1);
-			else if(i == 'S')newlocation = mp(location.first,location.second-1);
-			else if(i == 'W')newlocation = mp(location.first-1,location.second);
-			else if(i == 'E')newlocation = mp(location.first+1,location.second);
-			output += crossproduct(newlocation,location);
-			location = newlocation;
+void test_case(){	
+	vector<vector<bool>> fence(500,vector<bool>(500,false));
+	string s; cin>>s;
+	n = s.length();
+	int x = 250, y = 250;
+	fence[x][y] = true;
+	map<pair<int,int>,char> fencedir;
+	
+	for(int i = 0; i < n; i++){
+		if(s[i] == 'S'){
+			fence[x+1][y] = fence[x+2][y] = true;
+			fencedir[{x+1,y}] = 'S';
+			x += 2;
 		}
-		if(output > 0)print("CW");
-		else print("CCW");
-		
-		
+		else if(s[i] == 'N'){
+			fence[x-1][y] = fence[x-2][y] = true;
+			fencedir[{x-1,y}] = 'N';
+			x -= 2;
+		}
+		else if(s[i] == 'W'){
+			fence[x][y-1] = fence[x][y-2] = true;
+			fencedir[{x,y-1}] = 'W';
+			y -= 2;
+		}
+		else if(s[i] == 'E'){
+			fence[x][y+1] = fence[x][y+2] = true;
+			fencedir[{x,y+1}] = 'E';
+			y += 2;
+		}
+	}
+	fence[x][y] = true;
+	for(int i = 0; i < 8; i++){
+		int nr = dx[i] + 250, nc = dy[i]+250;
+		if(!fence[nr][nc]){
+			ok = true;
+			vector<vector<bool>> visited(500,vector<bool>(500,false));
+			ff(nr,nc,fence,visited);
+			if(ok){
+				for(int i = 0; i < 500; i++){
+					for(int j = 0; j < 500; j++){
+						if(fencedir.count({i,j})){
+							char dir = fencedir[{i,j}];
+							debug(dir);
+							if(visited[i][j+1]){
+								if(dir == 'N'){
+									cout<<"CW"<<"\n";
+									return;
+								}
+								if(dir == 'S'){
+									cout<<"CCW"<<"\n";
+									return;
+								}
+							}
+							if(visited[i][j-1]){
+								if(dir == 'N'){
+									cout<<"CCW"<<"\n";
+									return;
+								}
+								if(dir == 'S'){
+									cout<<"CW"<<"\n";
+									return;
+								}
+							}
+							if(visited[i+1][j]){
+								if(dir == 'E'){
+									cout<<"CW"<<"\n";
+									return;
+								}
+								if(dir == 'W'){
+									cout<<"CCW"<<"\n";
+									return;
+								}
+							}
+							if(visited[i-1][j]){
+								if(dir == 'W'){
+									cout<<"CW"<<"\n";
+									return;
+								}
+								if(dir == 'E'){
+									cout<<"CCW"<<"\n";
+									return;
+								}
+							}
+						}
+					}
+				}
+				
+				
+			}
+		}
 		
 	}
 	
-	
-	
 }
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	solve();
-    
-
-    return 0;
+main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t; cin>>t;
+	while(t--){
+		test_case();
+	}
+	
 }
