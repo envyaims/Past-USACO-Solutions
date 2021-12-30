588aa82cec66634f507f2f7ac2f40a7bee7bf796
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-29 21:23:17 -0500

    Create haircut.cpp

---
+++ gold/haircut.cpp
@@ -0,0 +1,85 @@
+// Author: envy <3 uwu
+// Problem: Problem 1. Haircut
+// Attempted: 2021-12-29 21:04:31 EST
+
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define vt vector
+#define pb push_back
+#define ins insert
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define F first
+#define S second
+#define cont continue
+#define SZ(x) (int)x.size()
+#define pf printf
+#define retf return false
+#define rett return true
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FORE(i,a,b) for (int i = (a); i <= (b); ++i)
+#define ROF(i,a,b) for (int i = (a); i > (b); --i)
+#define ROFE(i,a,b) for (int i = (a); i >= (b); --i)
+#define trav(a,x) for (auto& a: x)
+#define rv(x) trav(i,x)cin>>i;
+
+template<class T> using maxpq = priority_queue<T>;
+template<class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> bool ckmin(T& a, const T& b){return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a, const T& b){return a<b?a=b,1:0;}
+
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b>>=1;a=(a*a)%m;}return ans;}
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
+const int MOD = 1e9+7;
+const int nax = 1e5;
+
+set<int> stop[nax+1];
+
+void solve(){
+	ifstream cin("haircut.in");
+	ofstream cout("haircut.out");
+	int n; cin>>n;
+	FOR(i,0,n){
+		int a; cin>>a;
+		stop[a].insert(i);
+	}
+	vt<int> ans(n);
+	ordered_set<int> dead;
+	FOR(i,1,n){
+		trav(j,stop[i-1]){
+			int bef = dead.order_of_key(j);
+			ans[i] += j-bef;
+			dead.ins(j);
+		}
+	}
+	FOR(i,1,n){
+		ans[i] += ans[i-1];
+	}
+	FOR(i,0,n){
+		cout<<ans[i]<<"\n";
+	}
+	
+}
+
+main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1; 
+	//cin>>t;
+	while(t--){
+		solve();
+	}
+	
+}
