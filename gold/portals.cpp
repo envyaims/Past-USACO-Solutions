2daf8a91ab412e43c54f09ae82ac2392d0a84cea
envifly <79723780+envyaims@users.noreply.github.com>
2021-12-31 19:48:00 -0500

    Add files via upload

---
+++ gold/portals.cpp
@@ -0,0 +1,107 @@
+// Author: envy <3 uwu
+// Problem: Problem 2. Portals
+// Attempted: 2021-12-31 15:55:55 EST
+
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define vt vector
+#define pb push_back
+#define ins insert
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define F first
+#define S second
+#define cont continue
+#define SZ(x) (int)x.size()
+#define pf printf
+#define retf return false
+#define rett return true
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FORE(i,a,b) for (int i = (a); i <= (b); ++i)
+#define ROF(i,a,b) for (int i = (a); i > (b); --i)
+#define ROFE(i,a,b) for (int i = (a); i >= (b); --i)
+#define trav(a,x) for (auto& a: x)
+#define read(x) trav(i,x)cin>>i;
+
+template<class T> using maxpq = priority_queue<T>;
+template<class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> bool ckmin(T& a, const T& b){return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a, const T& b){return a<b?a=b,1:0;}
+
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b>>=1;a=(a*a)%m;}return ans;}
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
+const int MOD = 1e9+7;
+
+struct DSU{
+	vector<int> p, sz;
+	DSU(int n){
+		p.resize(n);
+		sz.resize(n,1);
+		iota(p.begin(),p.end(),0);
+	}
+	int get(int x){
+		if(p[x] != x){
+			p[x] = get(p[x]);
+		}
+		return p[x];
+	}
+	bool unite(int a, int b){
+		a = get(a); b = get(b);
+		if(sz[a] < sz[b])swap(a,b);
+		if(a != b){
+			p[b] = a;
+			sz[a] += sz[b];
+		}
+		return a != b;
+	}
+	bool sameset(int a, int b){return get(a) == get(b);}
+	int getsize(int a){return sz[get(a)];}
+};
+
+void solve(){
+	int n; cin>>n;
+	DSU dsu(2*n);
+	vt<array<int,3>> edg;
+	FOR(i,0,n){
+		int c, pa, pb, pc, pd;
+		cin>>c>>pa>>pb>>pc>>pd;
+		--pa; --pb; --pc; --pd;
+		edg.pb({0,pa,pb});
+		edg.pb({0,pc,pd});
+		edg.pb({c,pa,pc});
+	}
+	sort(all(edg));
+	int ans = 0;
+	trav(i,edg){
+		if(dsu.unite(i[1],i[2])){
+			ans += i[0];
+		}
+		if(dsu.getsize(1) == 2*n){
+			break;
+		}
+	}
+	pf("%lld",ans);
+}
+
+main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1; 
+	//cin>>t;
+	while(t--){
+		solve();
+	}
+	
+}
