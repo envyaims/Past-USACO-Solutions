69ce10dd21523c7c2508efee2b1d1c3401cf1642
envifly <79723780+envyaims@users.noreply.github.com>
2022-01-03 21:34:12 -0500

    Update balancing.cpp

--- bronze/balancing.cpp
+++ bronze/balancing.cpp
@@ -1,87 +1,91 @@
+// Author: envy <3 uwu
 // Problem: Problem 3. Load Balancing
-// Contest: USACO - USACO 2016 February Contest, Bronze
-// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=617
-// Memory Limit: 256 MB
-// Time Limit: 4000 ms
-// Input: file balancing.in  
-// Output: file balancing.out  
-// Attempted: 2021-07-30 12:27:39 EST
-// 7/10 test cases :(
+// Attempted: 2022-01-03 21:02:09 EST
 
 #include <bits/stdc++.h>
-
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "C:\programmingfunnyxd\debug.cpp"
+#endif
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef set<int> si;
-typedef pair<int,int> pi;
-
-const ll INF = 1e18;
-const int MOD = 1e9+7;
-#define all(x) x.begin(), x.end()
-#define pb(x) push_back(x)
-#define debug(x) cout<<#x<<" = "<<x<<endl
-#define print(x) cout<<x<<endl
-#define mp(x,y) make_pair(x,y)
-#define np(x) next_permutation(all(x))
+#define int long long
+#define vt vector
+#define pb push_back
+#define ins insert
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
 #define F first
 #define S second
-#define sz(v) int(v.size())
+#define cont continue
+#define SZ(x) (int)x.size()
+#define pf printf
+#define retf return false
+#define rett return true
 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define FORE(i,a,b) for (int i = (a); i <= (b); ++i)
+#define ROF(i,a,b) for (int i = (a); i > (b); --i)
+#define ROFE(i,a,b) for (int i = (a); i >= (b); --i)
+#define trav(a,x) for (auto& a: x)
+#define read(x) trav(i,x)cin>>i;
 
-void doStuff(){
-	ifstream cin("balancing.in");
+template<class T> using maxpq = priority_queue<T>;
+template<class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> bool ckmin(T& a, const T& b){return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a, const T& b){return a<b?a=b,1:0;}
+
+int pow(int a,int b,int m){int ans=1;while(b){if(b&1){ans=(ans*a)%m;}b>>=1;a=(a*a)%m;}return ans;}
+const int dx[8] = {1,-1,0,0,-1,-1,1,1}, dy[8] = {0,0,-1,1,1,-1,-1,1};
+const int MOD = 1e9+7;
+
+void solve(){
+    ifstream cin("balancing.in");
 	ofstream cout("balancing.out");
-	int n,b; cin>>n>>b;
-	vector<pair<int,int>> cowpos;
-	int maxa = 0;
-	int maxb = 0;
-	int mina = INT_MAX;
-	int minb = INT_MAX;
-	for(int i = 0; i < n; i++){
-		int a,b; cin>>a>>b;
-		cowpos.pb(mp(a,b));
-		maxa = max(maxa,a);
-		maxb = max(maxb,b);
-		mina = min(mina,a);
-		minb = min(minb,b);
+	int n,m; cin>>n>>m;
+	vt<pair<int,int>> cow(n);
+	trav(i,cow){
+		cin>>i.F>>i.S;
 	}
-	int ans = INT_MAX;
 	
-	for(int i = mina+1; i < maxa-2; i+=2){
-	 	for(int j = minb+1; j < maxb-2; j += 2){
-	 		// divider at i,j
-	 		int bottomleft = 0;
-	 		int bottomright = 0;
-	 		int topleft = 0;
-	 		int topright = 0;
-	 		for(auto k: cowpos){
-	 			if(k.F < i && k.S < j){
-	 				bottomleft++;
-	 			}
-	 			else if(k.F > i && k.S < j){
-	 				bottomright++;
-	 			}
-	 			else if(k.F < i && k.S > j){
-	 				topleft++;
-	 			}
-	 			else topright++;
-	 		}
-	 		ans = min(ans,max({bottomleft,bottomright,topleft,topright}));
-	 		
-	 	}
+	int ans = 1e9;
+	auto testfence = [&](int x, int y){
+		vt<int> cnt(4);
+		trav(i,cow){
+			if(i.F < x){
+				if(i.S < y)cnt[0]++;
+				else cnt[1]++;
+			}
+			else{
+				if(i.S < y)cnt[2]++;
+				else cnt[3]++;
+			}
+		}
+		ckmin(ans,*max_element(all(cnt)));
+	};
+	
+	trav(i,cow){
+		trav(j,cow){
+			FOR(k,4,8){
+				testfence(i.F+dx[k],j.S+dy[k]);
+			}
+		}
 	}
-	print(ans);
-	
-	
+	cout<<ans;
 }
 
-int main(){
-	ios_base::sync_with_stdio(false);
+main(){
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-	doStuff();
-    
-
-    return 0;
+	int t = 1; 
+	//cin>>t;
+	while(t--){
+		solve();
+	}
+	
 }
