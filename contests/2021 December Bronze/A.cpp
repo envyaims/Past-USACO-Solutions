95ccf4bfdece13bf6b275a3fcdb2e991a125123e
envifly <79723780+envyaims@users.noreply.github.com>
2022-01-21 19:26:48 -0500

    Update A.cpp

--- contests/2021 December Bronze/A.cpp
+++ contests/2021 December Bronze/A.cpp
@@ -3,25 +3,26 @@ using namespace std;
 #define int long long
 
 main(){
-	int n; cin>>n;
-	string s; cin>>s;
-	vector<vector<int>> cows(2);
-	for(int i = 0; i < n; i++){
-		s[i] == 'G' ? cows[0].push_back(i) : cows[1].push_back(i);
-	}
-	int ans = 0;
-	for(int i = 0; i < 2; i++){
-		for(int j = 0; j < cows[i].size(); j++){
-			int lft = -1, rgt = n;
-			if(j)lft = cows[i][j-1];
-			if(j < cows[i].size()-1)rgt = cows[i][j+1];
-			int l = cows[i][j]-lft-1;
-			int r = rgt-cows[i][j]-1;
-			ans += (l+1)*(r+1)-1;
-			if(l)ans--;
-			if(r)ans--;
-		}
-	}
-	printf("%lld",ans);
-	
+    int n; cin>>n;
+    string s; cin>>s;
+    vector<vector<int>> cows(2);
+    for(int i = 0; i < n; i++){
+        s[i] == 'G' ? cows[0].push_back(i) : cows[1].push_back(i);
+    }
+    int ans = 0;
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < cows[i].size(); j++){
+            int lft = -1, rgt = n;
+            if(j)lft = cows[i][j-1]; // if not first cow, get the left cow with same breed
+            if(j < cows[i].size()-1)rgt = cows[i][j+1]; // if not last cow...
+            int l = cows[i][j]-lft-1;
+            int r = rgt-cows[i][j]-1;
+            ans += (l+1)*(r+1)-1; // (dist from left)*(dist from right) - overcounted current cow 
+            // double counted the left and the right segments
+            if(l)ans--;
+            if(r)ans--;
+        }
+    }
+    printf("%lld",ans);
+    
 }
